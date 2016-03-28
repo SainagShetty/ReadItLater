8624fbf07acd2ce0c68cde0ec6e65e23dcfb1b7f
sainag-shetty <shettysainag@gmail.com>
2016-03-28 19:34:58 +0530

    Config file added

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	$host="localhost"; // Host name 
+	$username="root"; // Mysql username 
+	$password=""; // Mysql password 
+	$db_name="login"; // Database name 
+	$tbl_name="members"; // Table name
+	$salt = "batman is here"; // For SHA1 hashing
+	$tbl_name_art = "articles"; //tabel name where articles are stored
+	$apikey = "YOUR_ALCHEMY_API_HERE";
+
+?>
